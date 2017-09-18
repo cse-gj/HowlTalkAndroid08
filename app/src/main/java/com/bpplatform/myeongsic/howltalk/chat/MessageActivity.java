4d2c7c9a4103fe5017fe48b5e51064ccc8122ee6
MyeongSic <you6878@icloud.com>
2017-09-18 21:12:12 +0900

    init

---
+++ app/src/main/java/com/bpplatform/myeongsic/howltalk/chat/MessageActivity.java
@@ -0,0 +1,15 @@
+package com.bpplatform.myeongsic.howltalk.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bpplatform.myeongsic.howltalk.R;
+
+public class MessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message2);
+    }
+}
